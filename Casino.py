3902bb40bc0206452240917f2ddcdaed3f1ab0b8
Dazovscky <dazovsckyao@gmail.com>
2021-10-22 19:49:42 +0300

    Add files via upload

---
+++ Casino.py
@@ -0,0 +1,36 @@
+from random import *
+
+number1 = [1, 2, 3]
+number2 = [1, 2, 3]
+number3 = [1, 2, 3]
+cash = int(input("Enter your deposit UAH :"))
+
+
+def line1():
+    shuffle(number1)
+    shuffle(number2)
+    shuffle(number3)
+    print(number1)
+    print(number2)
+    print(number3)
+
+
+while True:
+    game = input("Spin? y/n :")
+    if game == "y":
+        line1()
+        if number1 == number2:
+            cash += 25
+            print("Your win :", cash)
+        elif number2 == number3:
+            cash += 25
+            print("Your win :", cash)
+        else:
+            cash -= 25
+            print("Your loose :", cash)
+            if cash <= 0:
+                print("Game over :", cash)
+                break
+    else:
+        print("Game over", cash)
+        break
